ae2dd92600c33e230718cf92514a3cf5c35eaa45
zyanyato <zyannya.to@gmail.com>
2024-11-07 18:15:02 -0600

    empty

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    VITE_API_URL: string; VITE_GITHUB_TOKEN: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
